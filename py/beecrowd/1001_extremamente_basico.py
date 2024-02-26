2a6cfedd2ea9ba75e3d60fec115036db013e2473
hgrafa <hugorafell@gmail.com>
2024-02-26 15:52:24 -0300

    update

---
+++ py/beecrowd/1001_extremamente_basico.py
@@ -0,0 +1,12 @@
+a = int(input())
+
+b = int(input())
+
+soma = a + b
+
+print(f'X = {soma}')
+
+# declarando a -> conversor de inteiros -> entrada do usuario
+# entrada do usuario retorna 'str'
+# conversor de inteiros espera receber um 'str' e retorna 'int'
+# declaro a com o retorno do conversor de inteiros
